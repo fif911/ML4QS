97692558090079ba60bfb938d360fc2a50315773
MikCas <mikhail.cassar@gmail.com>
2023-06-08 12:34:47 +0200

    wip

--- Python3Code/phyphox_prepare_dataset.py
+++ Python3Code/phyphox_prepare_dataset.py
@@ -61,6 +61,13 @@ for experiment_name in experiment_names:
 ####################################################################
 #Data Aggregation
 
+# print(timestamped_datasets)
+#Dictionary keys - the files of the datasets
+files = timestamped_datasets.keys()
+
+print(files[0])
+exit(0)
+
 # Define the activities and measurements
 activities = ['walking', 'running', 'cycling', 'sitting', 'hammocking']
 measurements = ['Accelerometer', 'Gyroscope', 'Light', 'Linear Acceleration', 'Location', 'Magnetometer', 'Proximity']
