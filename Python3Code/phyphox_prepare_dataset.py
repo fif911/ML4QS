702f23f8095c79aa96363ef47e139bef74e6ead1
Oleksandr Zakotianskyi <sandrrichfif911@gmail.com>
2023-06-08 20:11:52 +0200

    (prepare_dataset): format code

--- Python3Code/phyphox_prepare_dataset.py
+++ Python3Code/phyphox_prepare_dataset.py
@@ -27,8 +27,7 @@ _USER_FOLDER = "marie-dataset"
 USER_PATH = Path(f'./datasets/phyphox/{_USER_FOLDER}')
 
 RESULT_PATH = Path('./phyphox-outputs/')
-RESULT_FNAME = f'{_USER_FOLDER}_AGGREGATED_BY_TYPE.csv' # TODO: Add type
-
+RESULT_FNAME = f'{_USER_FOLDER}_AGGREGATED_BY_TYPE.csv'  # TODO: Add type
 
 # Set a granularity (the discrete step size of our time series data). We'll use a course-grained granularity of one
 # instance per minute, and a fine-grained one with four instances per second.
@@ -52,13 +51,12 @@ for experiment_name in experiment_names:
         timestamp_offset = CreateDataset.get_timestamp_offset(USER_PATH / experiment_name)
 
         # Ignore meta files
-        if file_name not in ["meta","device.csv","time.csv"]:
+        if file_name not in ["meta", "device.csv", "time.csv"]:
             print(f"Preparing {file_name}")
             dataset = CreateDataset.prepare_dataset(USER_PATH / experiment_name / file_name, timestamp_offset)
             timestamped_datasets[f"{experiment_name}:{file_name}"] = dataset
         else:
             print(f"Skipping {file_name}")
-
 
 # pprint(timestamped_datasets)
 # timestamped_datasets ={
@@ -69,7 +67,7 @@ for experiment_name in experiment_names:
 # }
 
 ####################################################################
-#Data Aggregation
+# Data Aggregation
 
 # Define the activities and measurements
 activities = ['walking', 'running', 'cycling', 'sitting', 'hammocking']
@@ -82,7 +80,7 @@ for measurement in measurements:
     measurement_data = {}
     combined_measurement = pd.DataFrame()
 
-    print('\n') 
+    print('\n')
 
     # Iterate over each activity
     for activity in activities:
@@ -94,7 +92,7 @@ for measurement in measurements:
         # Iterate over each file in the dictionary
         for file in timestamped_datasets:
             print(file)
-            
+
             # if measurement in file and activity in file:
             #     # Read the Excel file into a DataFrame
             #     file_path = os.path.join(directory, file)
@@ -103,39 +101,47 @@ for measurement in measurements:
             # Check if the file contains the current measurement and activity
             if measurement in file and activity in file:
                 print('True')
-                #Get the data from the dictionary 
+                # Get the data from the dictionary
                 data = timestamped_datasets[file]
-                data.insert(len(data.columns), 'label', len(data.index)*[activity], True)
+                data.insert(len(data.columns), 'label', len(data.index) * [activity], True)
                 # print(data)
-                
+
                 combined_data = combined_data.append(data)
 
         # print("combined data\n")
         # print(combined_data)
         combined_measurement = combined_measurement.append(combined_data)
-    
+
     print("combined measurement\n")
     print(combined_measurement)
     # measurement_data[measurement] = combined_measurement
     # ['Accelerometer', 'Gyroscope', 'Light', 'Linear Acceleration', 'Location', 'Magnetometer']
     if (measurement == 'Accelerometer'):
-        combined_measurement = combined_measurement.rename(columns={'Acceleration x (m/s^2)': 'x', 'Acceleration y (m/s^2)': 'y', 'Acceleration z (m/s^2)': 'z'})
+        combined_measurement = combined_measurement.rename(
+            columns={'Acceleration x (m/s^2)': 'x', 'Acceleration y (m/s^2)': 'y', 'Acceleration z (m/s^2)': 'z'})
     elif (measurement == 'Gyroscope'):
-        combined_measurement.rename(columns={'Gyroscope x (rad/s)': 'x', 'Gyroscope y (rad/s)': 'y', 'Gyroscope z (rad/s)': 'z'}, inplace=True)
+        combined_measurement.rename(
+            columns={'Gyroscope x (rad/s)': 'x', 'Gyroscope y (rad/s)': 'y', 'Gyroscope z (rad/s)': 'z'}, inplace=True)
     elif (measurement == 'Light'):
         combined_measurement.rename(columns={'Illuminance (lx)': 'lux'}, inplace=True)
     elif (measurement == 'Linear Acceleration'):
-        combined_measurement.rename(columns={'Linear Acceleration x (m/s^2)': 'x', 'Linear Acceleration y (m/s^2)': 'y', 'Linear Acceleration z (m/s^2)': 'z'}, inplace=True)
+        combined_measurement.rename(columns={'Linear Acceleration x (m/s^2)': 'x', 'Linear Acceleration y (m/s^2)': 'y',
+                                             'Linear Acceleration z (m/s^2)': 'z'}, inplace=True)
     elif (measurement == 'Location'):
-        combined_measurement.rename(columns={'Latitude (Â°)': 'latitude', 'Longitude (Â°)': 'longitude', 'Velocity (m/s)': 'speed', 'Height (m)': 'height', 'Direction (Â°)': 'direction', 'Horizontal Accuracy (m)': 'horizontal_accuracy', 'Vertical Accuracy (m)': 'vertical_accuracy'}, inplace=True)
+        combined_measurement.rename(
+            columns={'Latitude (Â°)': 'latitude', 'Longitude (Â°)': 'longitude', 'Velocity (m/s)': 'speed',
+                     'Height (m)': 'height', 'Direction (Â°)': 'direction',
+                     'Horizontal Accuracy (m)': 'horizontal_accuracy', 'Vertical Accuracy (m)': 'vertical_accuracy'},
+            inplace=True)
     elif (measurement == 'Magnetometer'):
-        combined_measurement.rename(columns={'Magnetic field x (ÂµT)': 'x', 'Magnetic field y (ÂµT)': 'y', 'Magnetic field z (ÂµT)': 'z'}, inplace=True)
+        combined_measurement.rename(
+            columns={'Magnetic field x (ÂµT)': 'x', 'Magnetic field y (ÂµT)': 'y', 'Magnetic field z (ÂµT)': 'z'},
+            inplace=True)
     else:
         pass
 
     print("combined measurement\n")
     print(combined_measurement)
-
 
     combined_measurement = combined_measurement.sort_values(by='timestamps')
 
