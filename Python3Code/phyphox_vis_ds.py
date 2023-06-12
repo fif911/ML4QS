4e97dfad19eb3946866f517cb1c0d5b7ae7cb69c
Oleksandr Zakotianskyi <sandrrichfif911@gmail.com>
2023-06-12 13:45:21 +0200

    add latest changes related to outlier detection and dataset visulizations

--- Python3Code/phyphox_vis_ds.py
+++ Python3Code/phyphox_vis_ds.py
@@ -3,7 +3,9 @@ from util.VisualizeDataset import VisualizeDataset
 
 import pandas as pd
 
-dataset = pd.read_csv("phyphox-outputs/chapter2_result_250_labeled_aligned.csv", skipinitialspace=True)
+dataset = pd.read_csv("phyphox-outputs/chapter2_result_250_labeled_aligned_min_extended_onehot.csv",
+                      skipinitialspace=True)
+dataset = dataset.iloc[4500:4550, :]  # get first 1000 rows
 
 # Plot the data
 DataViz = VisualizeDataset(__file__)
@@ -19,14 +21,18 @@ DataViz = VisualizeDataset(__file__)
 #                      # ['line', 'line', 'line', 'line', 'line', 'line', 'points', 'points']
 #                      )
 
+
+# Full DS plot
+# DataViz.plot_dataset(dataset,
+#                      ['acc', 'lin_acc', 'loc_speed', 'gyr', 'loc_L', ],
+#                      ['like', 'like', 'like', 'like', 'like', 'exact', 'like'],
+#                      ['line', 'line', 'line', 'line', 'like', 'line', 'line']
+#                      )
+# Minimal DS plot
 DataViz.plot_dataset(dataset,
-                     ['acc', 'lin_acc', 'loc_speed', 'gyr', 'loc_L'],
-                     ['like', 'like', 'exact', 'like', 'like', 'exact', 'like'],
-                     ['line', 'line', 'line', 'line', 'like', 'line', 'line']
-                     # ['acc_', 'gyr_', 'hr_watch_rate', 'light_phone_lux', 'mag_', 'press_phone_', 'label'],
-                     # ['like', 'like', 'like', 'like', 'like', 'like', 'like', 'like'],
-                     # ['line', 'line', 'line', 'line', 'line', 'line', 'points', 'points']
+                     ['acc', 'lin_acc', 'loc', 'label_'],
+                     ['like', 'like', 'like', 'like'],
+                     ['line', 'line', 'line', 'points']
                      )
 
 print("done")
-exit(0)
