419d6809619d574fdda19a6c613456324dedd84f
Oleksandr Zakotianskyi <sandrrichfif911@gmail.com>
2023-06-14 12:23:07 +0200

    fix outlier detection script and add pca script

--- Python3Code/phyphox_utils/outlier_detection_script.py
+++ Python3Code/phyphox_utils/outlier_detection_script.py
@@ -3,11 +3,13 @@ import copy
 import numpy as np
 import pandas as pd
 
-dataset = pd.read_csv("../phyphox-outputs/chapter2_result_250_labeled_aligned_min_extended_onehot.csv",
+from util.VisualizeDataset import VisualizeDataset
+
+FILE_NAME = "ds_250_labeled_aligned_onehot.csv"
+dataset = pd.read_csv(f"../phyphox-outputs/{FILE_NAME}",
                       skipinitialspace=True)
 print(dataset.isnull().sum())
 
-from Python3Code.util.VisualizeDataset import VisualizeDataset
 from Python3Code.Chapter3.OutlierDetection import DistributionBasedOutlierDetection, DistanceBasedOutlierDetection
 
 # Create the outlier classes.
@@ -15,7 +17,9 @@ OutlierDistr = DistributionBasedOutlierDetection()
 DataViz = VisualizeDataset("outlier_detection")
 from typing import List
 
-outlier_columns = ["lin_acc_x", "lin_acc_y", "lin_acc_z", "loc_speed"]
+outlier_columns = ["lin_acc_x", "lin_acc_y", "lin_acc_z", "loc_speed", "gyr_x", "gyr_y", "gyr_z",
+                   "loc_horizontal_accuracy", "loc_vertical_accuracy", "mang_field_x", "mang_field_y", "mang_field_z"
+                   ]
 activities = dataset["label"].unique()
 list_of_cleaned_datasets_by_activity: List = []
 for activity in activities:
@@ -24,7 +28,6 @@ for activity in activities:
 
     for col in outlier_columns:
         print(f"Applying Chauvenet outlier criteria for activity {activity} column {col}")
-        # TODO: skip for cycling loc_speed
         before_outlier_detection = copy.deepcopy(dataset_by_activity)
         dataset_by_activity = OutlierDistr.chauvenet(dataset_by_activity, col, 2)
 
@@ -41,7 +44,7 @@ concatened_dataset = concatened_dataset.sort_values(by=['time'])
 # search for NaN
 print(concatened_dataset.isnull().sum())
 # save to csv
-save_path = "../phyphox-outputs/chapter2_result_250_labeled_aligned_min_extended_onehot_outlier.csv"
+save_path = f"../phyphox-outputs/{FILE_NAME}_outliers_removed.csv"
 concatened_dataset.to_csv(save_path,
                           index=False)
 print(save_path)
